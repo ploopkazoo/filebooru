992aabb71730bec688c3a0a4e42c66badc4cd58a
Lucas Phillips <mail@lep.pw>
2016-01-12 15:24:19 -0500

    added human_size() for conversion of bytes to larger units

--- filebooru.py
+++ filebooru.py
@@ -5,6 +5,16 @@ from filebooru_settings import fb_confd, fb_conn, fb_common
 
 app = Flask(__name__)
 
+def human_size(bytecount):
+    itters = 0
+    while bytecount >= 1024:
+        itters += 1
+        bytecount /= 1024
+    if itters == 0:
+        return "%i %s" % (bytecount, "B")
+    else:
+        return "%.2f %s" % (bytecount, ["B", "KiB", "MiB", "GiB", "TiB", "PiB"][itters])
+
 @app.route("/file/<fileid>")
 def render_root(fileid):
     return render_template("file.html")
