b8d599a5c246c8a56fbe499affdb66600d7bf691
Lucas Phillips <mail@lep.pw>
2016-03-17 10:54:39 -0400

    added LibreJS compatibility comments

--- static/js/jquery-2.2.1.js
+++ static/js/jquery-2.2.1.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
 /*!
  * jQuery JavaScript Library v2.2.1
  * http://jquery.com/
@@ -9829,3 +9830,4 @@ if ( !noGlobal ) {
 
 return jQuery;
 }));
+// @license-end
